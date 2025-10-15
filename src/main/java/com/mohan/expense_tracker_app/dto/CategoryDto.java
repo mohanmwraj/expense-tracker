9ffa5b06a263abed655aa6ca0d46c76a4eea06a1
Mohan Raj <mohanmwraj@gmail.com>
2025-10-16 01:28:45 +0530

    Added description for the Schema

--- src/main/java/com/mohan/expense_tracker_app/dto/CategoryDto.java
+++ src/main/java/com/mohan/expense_tracker_app/dto/CategoryDto.java
@@ -1,5 +1,16 @@
 package com.mohan.expense_tracker_app.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+        description = "Category DTO ( Data Transfer Object) to transfer the data between the client and server"
+)
+
+
 //records to create a immutable class.
-public record CategoryDto(Long id, String name) {
+public record CategoryDto(Long id,
+                          @Schema(
+                                  description = "Category Name"
+                          )
+                          String name) {
 }
--- src/main/java/com/mohan/expense_tracker_app/exceptions/ErrorDetails.java
+++ src/main/java/com/mohan/expense_tracker_app/exceptions/ErrorDetails.java
@@ -1,16 +1,31 @@
 package com.mohan.expense_tracker_app.exceptions;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(
+        description = "ErrorDetails DTO (Data Transfer Object) to " +
+                "transfer the error response data between client and server"
+)
+
 @Getter
 @Setter
 public class ErrorDetails {
+
+    @Schema(description = "Error Occurred data time")
     private LocalDateTime timeStamp;
+
+    @Schema(description = "Error Message")
     private String message;
+
+    @Schema(description = "Error Details")
     private String details;
+
+    @Schema(description = "Error Code")
     private String errorCode;
 
 }
